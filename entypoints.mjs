0c7f725c898b86897ce90005f6ca1164a5880416
kata-list <katalist47@gmail.com>
2025-06-16 20:04:54 +0600

    rename to page-animation

--- entypoints.mjs
+++ entypoints.mjs
@@ -2,4 +2,4 @@
  * This file defines the entry points for the application.
  * Example: ['src/target_folder/target_file.ts', 'src/target_folder2/target_file2.ts']
  */
-export const ENTRY_POINTS = ["src/logo-morph-animation.ts", "src/page-animation-triggers.ts"];
+export const ENTRY_POINTS = ["src/logo-morph-animation.ts", "src/page-animations.ts"];
