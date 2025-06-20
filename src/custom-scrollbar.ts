7cdd77df36b0c2a8180a9b6c3dcbe41b06722666
kata-list <katalist47@gmail.com>
2025-06-21 03:32:41 +0600

    add missing plugin in custom scrollbar

--- src/custom-scrollbar.ts
+++ src/custom-scrollbar.ts
@@ -1,14 +1,19 @@
 import "overlayscrollbars/overlayscrollbars.css";
 
 import { afterWebflowReady, getMultipleHtmlElements } from "@taj-wf/utils";
-import { ClickScrollPlugin, OverlayScrollbars, SizeObserverPlugin } from "overlayscrollbars";
+import {
+  ClickScrollPlugin,
+  OverlayScrollbars,
+  ScrollbarsHidingPlugin,
+  SizeObserverPlugin,
+} from "overlayscrollbars";
 
 const initCustomScrollbar = () => {
   const scrollTargetElements = getMultipleHtmlElements({ selector: "[data-custom-scrollbar]" });
 
   if (!scrollTargetElements) return;
 
-  OverlayScrollbars.plugin([ClickScrollPlugin, SizeObserverPlugin]);
+  OverlayScrollbars.plugin([ClickScrollPlugin, SizeObserverPlugin, ScrollbarsHidingPlugin]);
 
   for (const element of scrollTargetElements) {
     OverlayScrollbars(element, {});
